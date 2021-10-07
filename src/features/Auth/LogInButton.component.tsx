e880276bd02889fb7667cb16ebb7f8a4ce0c8cb9
Vadim Marchenko <v3.marchenko@itransition.com>
2021-10-07 23:04:02 +0300

    Use async/await in logIn func and create auth url constant in LogInButton component

--- src/features/Auth/LogInButton.component.tsx
+++ src/features/Auth/LogInButton.component.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 
+import { THE_MOVIE_DB_AUTHENTICATION_URL } from "constants/api";
 import * as api from "./Auth.api";
 
 const { REACT_APP_AUTH_REDIRECT_URL } = process.env;
@@ -9,12 +10,12 @@ const { REACT_APP_AUTH_REDIRECT_URL } = process.env;
 export const LogInButton = () => {
   const { t } = useTranslation();
 
-  const logIn = () => {
-    api.createRequestToken().then((request_token) => {
-      window.location.replace(
-        `https://www.themoviedb.org/authenticate/${request_token}?redirect_to=${REACT_APP_AUTH_REDIRECT_URL}`
-      );
-    });
+  const logIn = async () => {
+    const request_token = await api.createRequestToken();
+
+    window.location.replace(
+      `${THE_MOVIE_DB_AUTHENTICATION_URL}/${request_token}?redirect_to=${REACT_APP_AUTH_REDIRECT_URL}`
+    );
   };
 
   return (

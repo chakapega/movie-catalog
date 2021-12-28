33da0dac1e58e0646c7c8b44289c4f0cb698f17f
Vadim Marchenko <v3.marchenko@itransition.com>
2021-12-28 16:07:21 +0300

    Remove react-query package

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 
 import "./i18n";
@@ -11,17 +10,13 @@ import { store } from "store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
-const queryClient = new QueryClient();
-
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
